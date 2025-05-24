1d365583223368163bb696e8e7f213d95c57155c
Krefit <gustavokrawczyk@gmail.com>
2025-05-24 01:18:29 -0300

    Update executar_coletas.bat

--- projeto_data_lake/executar_coletas.bat
+++ projeto_data_lake/executar_coletas.bat
@@ -1,18 +1,18 @@
 @echo off
 cd /d %~dp0
 
-@REM echo Iniciando coleta de símbolos da B3...
-@REM call .venv\Scripts\activate.bat
-@REM python.exe scripts\coleta_simbolos_b3.py
+echo Iniciando coleta de simbolos da B3...
+call .venv\Scripts\activate.bat
+python.exe scripts\utils\coleta_historico_b3.py
 
-echo Iniciando coleta de preços diários...
-python.exe scripts\coleta_precos_diarios_yfinance.py
+@REM echo Iniciando coleta de preços diários...
+@REM python.exe scripts\coleta_precos_diarios_yfinance.py
 
 @REM echo Iniciando coleta de preços intradiários...
 @REM python.exe scripts\coleta_precos_intradiarios_binance.py
 
-@REM echo Iniciando coleta de notícias...
-@REM python.exe scripts\coleta_noticias_gnews.py
+echo Iniciando coleta de notícias...
+python.exe scripts\utils\coleta_noticias_b3.py
 
 echo Coletas concluídas.
 pause
